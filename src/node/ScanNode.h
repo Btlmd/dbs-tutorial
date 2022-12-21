eb59dab9c26aebb7cf00287cdcd2d4292a98523a
c7w <cc7w@foxmail.com>
2022-12-21 14:17:25 +0800

    feature: finish index scan node

--- src/node/ScanNode.h
+++ src/node/ScanNode.h
@@ -10,6 +10,7 @@
 #include <system/WhereConditions.h>
 #include <record/DataPage.h>
 #include <record/TableMeta.h>
+#include <index/IndexFile.h>
 
 class TrivialScanNode : public OpNode {
 public:
@@ -53,8 +54,72 @@ private:
     FileID data_fd;
 };
 
-
+/*
+ ** Given [key_start, key_end], use index_file to find location at key_start first
+ * Once the record > key_end, stop
+ */
 class IndexScanNode : public OpNode {
+   public:
+    IndexScanNode(BufferSystem& buffer, std::shared_ptr<const TableMeta> table_meta,
+                    std::shared_ptr<FilterCondition> condition,
+                    FileID page_file, IndexFile& index_file, const std::shared_ptr<IndexField>& key_start, const std::shared_ptr<IndexField>& key_end) :
+                                            OpNode{{}}, buffer{buffer}, condition{std::move(condition)},
+                                            data_fd{page_file}, meta{std::move(table_meta)}, index_file{index_file},
+                                            key_start(key_start), key_end(key_end) {
+        iter = index_file.SelectRecord(key_start);
+    }
+
+    void Reset() override {
+        OpNode::Reset();
+        iter = index_file.SelectRecord(key_start);
+    }
+
+    [[nodiscard]] bool Over() const override {
+        return iter.first == -1;
+    }
+
+    RecordList Next() override {
+
+        auto slot_id = iter.second;
+        auto child_cnt = index_file.Page(iter.first)->ChildCount();
+
+        RecordList ret{};
+        for (; iter.second < child_cnt; ++iter.second) {
+            std::shared_ptr<IndexRecordLeaf> leaf_record = index_file.Select(iter);
+            assert (leaf_record != nullptr);
+
+            if (*(leaf_record->key) > *key_end) {
+                iter.first = -1;
+                return ret;
+            }
+
+            auto page = buffer.ReadPage(data_fd, leaf_record->page_id);
+            DataPage dp{page, *meta};
+
+            auto record{dp.Select(leaf_record->slot_id)};
+            if (record != nullptr && (condition == nullptr || (*condition)(record))) {
+                ret.push_back(record);
+            }
+        }
+
+        if (iter.second == child_cnt) {
+            iter.first = index_file.Page(iter.first)->NextPage();
+            iter.second = 0;
+        }
+
+        return std::move(ret);
+    }
+
+    ~IndexScanNode() override = default;
+
+   private:
+    BufferSystem &buffer;
+    IndexFile& index_file;
+    const std::shared_ptr<const TableMeta> meta;
+    const std::shared_ptr<FilterCondition> condition;
+    std::pair<PageID, TreeOrder> iter{std::make_pair<PageID, TreeOrder>(0, 0)};
+    FileID data_fd;
+    const std::shared_ptr<IndexField> key_start, key_end;
 
 };
 
